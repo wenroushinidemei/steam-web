8ec27a8f790897cc411fdbeb76bcd0a5a1f288e5
liyou <1826235370@qq.com>
2021-09-23 20:33:48 +0800

    登录业务

---
+++ code/app/controller/login.js
@@ -0,0 +1,31 @@
+const Controller = require('egg').Controller;
+// 负责登录验证。
+class LoginController extends Controller {
+    // 正常登录
+    async login() {
+        const { ctx } = this.ctx;
+        let res = await ctx.service.select.user(ctx.query.account,ctx.query.password);
+        if (res == null) {
+            // 未注册
+            ctx.body = {
+                code: 602,
+                msg: res,
+            };
+        } else if (res) {
+            // 返回登录成功信息
+            ctx.body = {
+                code: 600,
+                msg: res,
+            };
+        } else {
+            // 账号或密码错误
+            ctx.body = {
+                code: 601,
+                msg: res,
+            };
+        }
+
+    }
+}
+
+module.exports = LoginController;
