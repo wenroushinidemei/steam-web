6e79c7c00a3a8e887faa77dd551b28b9c3241517
liyou <1826235370@qq.com>
2021-09-24 17:29:02 +0800

    增加一下登录时的cookie配置

--- code/app/controller/login.js
+++ code/app/controller/login.js
@@ -12,7 +12,21 @@ class LoginController extends Controller {
                 msg: res,
             };
         } else if (res) {
-            // 返回登录成功信息
+            // 返回登录成功信息,并将信息在客户端持久化，禁止修改（待加密处理）
+            ctx.cookies.set(account, ctx.query.account, {
+                maxAge: 100000,
+                httpOnly: true,
+                signed: false,
+                encrypt: false,
+                domain: 'steam.web',
+              });
+              ctx.cookies.set(password, ctx.query.password, {
+                maxAge: 100000,
+                httpOnly: true,
+                signed: false,
+                encrypt: false,
+                domain: 'steam.web',
+              });
             ctx.body = {
                 code: 600,
                 msg: res,
@@ -25,7 +39,6 @@ class LoginController extends Controller {
             };
         }
     }
-    
 }
 
 module.exports = LoginController;
