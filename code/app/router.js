8ea18cbbabd9563617a911402407a0ecd21c18e0
liyou <1826235370@qq.com>
2021-09-28 12:39:59 +0800

    登录交互

--- code/app/router.js
+++ code/app/router.js
@@ -2,7 +2,7 @@ module.exports = app => {
     const { router, controller } = app;
     router.get('/', controller.home.show);
     // 用户登录请求
-    router.get('/login', controller.login.login);
+    router.post('/login', controller.login.login);
     // 不管具体部分，直接请求，携带上对的键名就能得到对应信息。
     // 从数据库查询模块中的游戏信息集合
     router.get('/gameModule', controller.game.selects);
