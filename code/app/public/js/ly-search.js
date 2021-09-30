3dafc669cdca145be79093f8b723d996584d8e33
liyou <1826235370@qq.com>
2021-09-30 08:46:25 +0800

    搜索功能部分完成

--- code/app/public/js/ly-search.js
+++ code/app/public/js/ly-search.js
@@ -1,1 +1,17 @@
+let params= {
+    gameName:'',
+}
+
 // 完成搜索功能，懒加载
+axios({
+    url:'/search',
+    baseURL:'http://steam.web:80/',
+    method:'get',
+    params:{
+        gameName:'',
+    }
+}).then(res=>{
+    console.log(res.data)
+}).catch(err=>{
+    console.log(err)
+})
---
+++ code/app/service/search.js
@@ -0,0 +1,12 @@
+const Service = require('egg').Service;
+// 负责和数据库进行查询的service层模块
+class SearchService extends Service {
+    // 查询游戏时具有各种组合限制
+    async game(config) {
+        let list = await this.app.mysql.select('indexgame',{
+            where: config
+        })
+        return list;
+    }
+}
+module.exports = SearchService;
