d6a75b2a02811d051697eed8c29344f4479be407
liyou <1826235370@qq.com>
2021-10-08 10:41:15 +0800

    更新cookie和部分跳转

--- code/app/controller/login.js
+++ code/app/controller/login.js
@@ -17,22 +17,27 @@ class LoginController extends Controller {
             };
         } else if (res) {
             // 返回登录成功信息,并将信息在客户端持久化，禁止修改（待加密处理）
-            ctx.cookies.set('account', ctx.request.body.account, {
-                maxAge: 100000,
-                path:'/',
-                httpOnly: false,
-                signed: false,
-                encrypt: false,
-                domain: 'steam.web',
-            });
+            // ctx.cookies.set('account', ctx.request.body.account, {
+            //     maxAge: 100000,
+            //     path:'/',
+            //     httpOnly: false,
+            //     signed: false,
+            //     encrypt: false,
+            //     // domain: 'steam.web',
+            // });
             // ctx.cookies.set('password', ctx.request.body.password, {
             //     maxAge: 100000,
             //     path:'/',
             //     httpOnly: false,
             //     signed: false,
             //     encrypt: false,
-            //     domain: 'steam.web',
+            //     // domain: 'steam.web',
+            //     domain: '192.168.6.14:80'
             // });
+            res.cookies = {
+                account:ctx.request.body.account,
+                password:ctx.request.body.password
+            }
             ctx.body = {
                 code: 600,
                 msg: res,
